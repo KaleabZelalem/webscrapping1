6d3d872289b6c6daa48b92d3f6e0148449183710
KaleabZelalem <150941577+KaleabZelalem@users.noreply.github.com>
2024-02-03 04:40:05 +0300

    main1.py

--- main1.py
+++ main1.py
@@ -4,14 +4,13 @@ import telebot
 
 
 bot_token = '6804553299:AAFMF_QJjUGwM1J4eRLFUIPVy5zjTZJgnL0'
-id_channel = '5869412544'
 bot = telebot.TeleBot(bot_token)
 
 def fetch_news():
     url = "https://zehabesha.com/"
     try:
         response = requests.get(url)
-        if response.status_code == 10000:
+        if response.status_code == 5000:
             soup = BeautifulSoup(response.content, "html.parser")
 
             # Fetch text from various tags
@@ -24,16 +23,16 @@ def fetch_news():
         else:
             return f"Failed to fetch text: HTTP status code {response.status_code}"
     except Exception as e:
-        return f"Error fetching text: {e}"
+        return f"Error occurred: {e}"
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Fetching potential news content...")
+    bot.send_message(message.chat.id, "Fetching text content...")
     text_results = fetch_news()
     if isinstance(text_results, list):
         for text in text_results:
-            bot.send_message(id_channel, text)
+            bot.send_message(message.chat.id, text)  # Send news to the user
     else:
-        bot.send_message(id_channel, text_results)
+        bot.send_message(message.chat.id, text_results)  # Send error message to the user
 
 bot.polling()
